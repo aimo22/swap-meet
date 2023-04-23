18334310c58340703af7fb02e49f5627dc5e4dbb
Aisha <amdahir1@gmail.com>
2023-04-23 12:20:31 -0500

    first wave done

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
+# from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def  __init__(self, inventory=None):
+        self.inventory = inventory or []
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    def remove(self, item):
+        if item not in self.inventory: return False
+        self.inventory.remove(item)
+        return item
+
+        
